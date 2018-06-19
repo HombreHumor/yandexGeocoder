107da3902cf175ddbdce6d9ed90fd973ef439aac
Igor Kramar <hombrehumor@gmail.com>
2018-06-19 22:52:06 +0600

    beta 0.5

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
-# yandexGeocoder
+# yandexGeocoderHelper
 A helper class for working with the json-response of the Yandex geocoder.
+
+## INSTALLATION
+For this class to work, call it in the initialization file of your project as follows:
+```php
+require_once($PATH_TO_FILE . '/yaGeocoderHelper.php');
+```
+Or use Composer by setting the composer.json file to autoload this class.
+
+## USING
+This class allows you to get a complete list of geodata using the coordinates or address as input.
+First, initialize the object:
+```php
+$someObj = new yaGeoHelp;
+```
+Then pass the input data to the object. It can be either coordinates (latitude, longitude):
+```php
+$someObj->setLoc(55.711157, 37.713985) // Send some coordinates of the point in Moscow
+```
+Either some address passed as a string:
+```php
+$someObj->setAddress('Rome, Lungotevere de Cenci') 
+```
+After that you can use any of the following methods to get the data you need:
+
+### getCoords()
+```php
+$someObj->getCoords(); // Getting the coordinates (width, longitude) 
+```
+
+### getFullAddress()
+```php
+$someObj->getFullAddress(); // Getting the full address (Country, region, city, street, house)
+```
+
+### getFormattedAddress()
+```php
+$someObj->getFormattedAddress(); // Getting formatted address (City, street, house) 
+```
+
+### getCountry()
+```php
+$someObj->getCountry(); // Getting country 
+```
+
+### getAdministrativeArea()
+```php
+$someObj->getAdministrativeArea(); // Getting administrative area 
+```
+
+### getSubAdministrativeArea()
+```php
+$someObj->getSubAdministrativeArea(); // Getting subadministrative area
+```
+
+### getCity()
+```php
+$someObj->getCity(); //Getting city
+```
+
+### getStreet()
+```php
+$someObj->getStreet(); // Getting street
+```
+
+### getHouse()
+```php
+$someObj->getHouse(); // Getting house
+```
+
+## Geocoder Yandex
+All rights to the Yandex geocoder belong to Yandex N.V. (https://yandex.ru/company/). This class is intended for the convenience of using the products of the company "Yandex", using their open api source.
---
+++ yaGeocoderHelper.php
@@ -0,0 +1,148 @@
+<?php
+/*
+All rights to the Yandex geocoder belong to Yandex N.V. (https://yandex.ru/company/)
+*/
+
+class yanGeoHelp {
+  private $inLoc;
+  private $inAddress;
+  private $coord;
+  private $fullAddress;
+  private $formattedAddress;
+  private $country;
+  private $administrativeArea;
+  private $subAdministrativeArea;
+  private $city;
+  private $street;
+  private $house;
+  private $key;
+
+  public function setLoc($param) {
+    $this->inLoc = $param;
+    $this->geocodeAddress;
+  }
+
+  public function setAddress($param) {
+    $this->inAddress = $param;
+    $this->geocodeCoords;
+  }
+
+  public function setKey($param) {
+    $this->key = $param;
+  }
+
+  public function getCoords() {
+    if($this->coord) {
+      return $this->coord;
+    }
+  }
+
+  public function getFullAddress() {
+    if($this->fullAddress) {
+      return $this->fullAddress;
+    }
+  }
+
+  public function getFormattedAddress() {
+    if($this->formattedAddress) {
+      return $this->formattedAddress;
+    }
+  }
+
+  public function getCountry() {
+    if($this->country) {
+      return $this->country;
+    }
+  }
+
+  public function getAdministrativeArea() {
+    if($this->administrativeArea) {
+      return $this->administrativeArea;
+    }
+  }
+
+  public function getSubAdministrativeArea() {
+    if($this->subAdministrativeArea) {
+      return $this->subAdministrativeArea;
+    }
+  }
+
+  public function getCity() {
+    if($this->city) {
+      return $this->city;
+    }
+  }
+
+  public function getStreet() {
+    if($this->street) {
+      return $this->street;
+    }
+  }
+
+  public function getHouse() {
+    if($this->house) {
+      return $this->house;
+    }
+  }
+
+  private function geocodeCoords() {
+    if (strpos($this->inLoc, ',') !== false) {
+      $arrayCoord = explode(',', $this->inLoc);
+      $coord = $arrayCoord[1] . ' ' .  $arrayCoord[0];
+    } else {
+      $coord = $this->inLoc;
+    };
+    if ($this->key) {
+      $checkedKey = $this->key;
+    } else {
+      $checkedKey = '...';
+    };
+    $params = array(
+      'geocode' => $coord,
+      'format' => 'json',
+      'results' => 1,
+      'key' => $checkedKey,
+    );
+    $response = json_decode(file_get_contents('http://geocode-maps.yandex.ru/1.x/?' . http_build_query($params, '', '&')));
+    if ($response->response->GeoObjectCollection->metaDataProperty->GeocoderResponseMetaData->found > 0) {
+        $this->fullAddress = $response->response->GeoObjectCollection->featureMember[0]->GeoObject->metaDataProperty->GeocoderMetaData->text;
+        $this->formattedAddress = $response->response->GeoObjectCollection->featureMember[0]->GeoObject->metaDataProperty->GeocoderMetaData->Address->formatted;
+        $this->country = $response->response->GeoObjectCollection->featureMember[0]->GeoObject->metaDataProperty->GeocoderMetaData->AddressDetails->Country->CountryName;
+        $this->administrativeArea = $response->response->GeoObjectCollection->featureMember[0]->GeoObject->metaDataProperty->GeocoderMetaData->AddressDetails->Country->AdministrativeArea->AdministrativeAreaName;
+        $this->subAdministrativeArea = $response->response->GeoObjectCollection->featureMember[0]->GeoObject->metaDataProperty->GeocoderMetaData->AddressDetails->Country->AdministrativeArea->SubAdministrativeArea->SubAdministrativeAreaName;
+        $this->city = $response->response->GeoObjectCollection->featureMember[0]->GeoObject->metaDataProperty->GeocoderMetaData->AddressDetails->Country->AdministrativeArea->SubAdministrativeArea->Locality->LocalityName;
+        $this->street = $response->response->GeoObjectCollection->featureMember[0]->GeoObject->metaDataProperty->GeocoderMetaData->AddressDetails->Country->AdministrativeArea->SubAdministrativeArea->Locality->Thoroughfare->ThoroughfareName;
+        $this->house = $response->response->GeoObjectCollection->featureMember[0]->GeoObject->metaDataProperty->GeocoderMetaData->AddressDetails->Country->AdministrativeArea->SubAdministrativeArea->Locality->Thoroughfare->Premise->PremiseNumber;
+        $nonFCoo = explode(',', $response->response->GeoObjectCollection->featureMember[0]->GeoObject->Point->pos);
+        $this->coord = $nonFCoo[1] . ', ' . $nonFCoo[0];
+    };
+  }
+
+  private function geocodeAddress() {
+    if ($this->key) {
+      $checkedKey = $this->key;
+    } else {
+      $checkedKey = $checkedKey;
+    };
+    $params = array(
+      'geocode' => $this->inAddress,
+      'format' => 'json',
+      'results' => 1,
+      'key' => '...',
+    );
+    $response = json_decode(file_get_contents('http://geocode-maps.yandex.ru/1.x/?' . http_build_query($params, '', '&')));
+    if ($response->response->GeoObjectCollection->metaDataProperty->GeocoderResponseMetaData->found > 0) {
+        $this->fullAddress = $response->response->GeoObjectCollection->featureMember[0]->GeoObject->metaDataProperty->GeocoderMetaData->text;
+        $this->formattedAddress = $response->response->GeoObjectCollection->featureMember[0]->GeoObject->metaDataProperty->GeocoderMetaData->Address->formatted;
+        $this->country = $response->response->GeoObjectCollection->featureMember[0]->GeoObject->metaDataProperty->GeocoderMetaData->AddressDetails->Country->CountryName;
+        $this->administrativeArea = $response->response->GeoObjectCollection->featureMember[0]->GeoObject->metaDataProperty->GeocoderMetaData->AddressDetails->Country->AdministrativeArea->AdministrativeAreaName;
+        $this->subAdministrativeArea = $response->response->GeoObjectCollection->featureMember[0]->GeoObject->metaDataProperty->GeocoderMetaData->AddressDetails->Country->AdministrativeArea->SubAdministrativeArea->SubAdministrativeAreaName;
+        $this->city = $response->response->GeoObjectCollection->featureMember[0]->GeoObject->metaDataProperty->GeocoderMetaData->AddressDetails->Country->AdministrativeArea->SubAdministrativeArea->Locality->LocalityName;
+        $this->street = $response->response->GeoObjectCollection->featureMember[0]->GeoObject->metaDataProperty->GeocoderMetaData->AddressDetails->Country->AdministrativeArea->SubAdministrativeArea->Locality->Thoroughfare->ThoroughfareName;
+        $this->house = $response->response->GeoObjectCollection->featureMember[0]->GeoObject->metaDataProperty->GeocoderMetaData->AddressDetails->Country->AdministrativeArea->SubAdministrativeArea->Locality->Thoroughfare->Premise->PremiseNumber;
+        $nonFCoo = explode(',', $response->response->GeoObjectCollection->featureMember[0]->GeoObject->Point->pos);
+        $this->coord = $nonFCoo[1] . ', ' . $nonFCoo[0];
+    };
+  }
+}
+?>
